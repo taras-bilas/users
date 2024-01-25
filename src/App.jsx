73a2021a25931425aba4e678a9593dd0f33d0a47
Taras Bilas <tarasbilas01@gmail.com>
2024-01-25 18:24:03 +0200

    changed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { UserList } from './components/UserList/UserList';
 import { UserInfo } from './components/UserInfo/UserInfo';
 
 export const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<UserList />} />
-          <Route path="/details" element={<UserInfo />} />
-        </Routes>
-      </Router>
-    </div>
+    <Routes>
+      <Route path="/" element={<UserList />} />
+      <Route path="/details" element={<UserInfo />} />
+    </Routes>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter } from 'react-router-dom';
 
 const element = document.querySelector('#root');
 
 createRoot(element).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 );
